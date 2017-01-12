856427fe24cbd162f6b8fa822c51441c07329d30
Eden Liu <eden90267@gmail.com>
2017-01-12 18:05:47 +0800

    modify code to IIFE

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,16 @@
-const bodyRef = document.querySelector('body');
-bodyRef.innerHTML = `
-  <div class="container">
-    <h1>不能再用 Facebook 了！！</h1>
-    <div>
-      <span class="countdown">距離聯考只剩：</span>
-      <span class="countdown" id="countdown"></span>
-    </div>
-    <br>
-    <a href="https://www.google.com"><button class="btn btn-default">快來用 Google XD</button></a>
-  </div>
-`;
-
 (function() {
+	const bodyRef = document.querySelector('body');
+	bodyRef.innerHTML = `
+      <div class="container">
+        <h1>不能再用 Facebook 了！！</h1>
+        <div>
+          <span class="countdown">距離聯考只剩：</span>
+          <span class="countdown" id="countdown"></span>
+        </div>
+        <br>
+        <a href="https://www.google.com"><button class="btn btn-default">快來用 Google XD</button></a>
+      </div>
+    `;
 	const target_date = new Date("June 7, 2017").getTime();
 	let days;
 	let hours;
@@ -24,17 +23,17 @@ bodyRef.innerHTML = `
 		var current_date = new Date().getTime();
 		var seconds_left = (target_date - current_date) / 1000;
 		if (seconds_left > 0) {
-	        days = parseInt(seconds_left / 86400);
-            seconds_left = seconds_left % 86400;
-         
-            hours = parseInt(seconds_left / 3600);
-            seconds_left = seconds_left % 3600;
-         
-            minutes = parseInt(seconds_left / 60);
-            seconds = parseInt(seconds_left % 60);
-         
-            // format countdown string + set tag value
-            countdown.innerHTML = days + "天, " + hours + "小時, " + minutes + "分, " + seconds + "秒";  
+			days = parseInt(seconds_left / 86400);
+			seconds_left = seconds_left % 86400;
+
+			hours = parseInt(seconds_left / 3600);
+			seconds_left = seconds_left % 3600;
+
+			minutes = parseInt(seconds_left / 60);
+			seconds = parseInt(seconds_left % 60);
+
+			// format countdown string + set tag value
+			countdown.innerHTML = days + "天, " + hours + "小時, " + minutes + "分, " + seconds + "秒";
 		} else {
 			countdown.innerHTML = '恭喜考完！XD';
 			clearInterval(init);
